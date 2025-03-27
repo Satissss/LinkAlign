5abd3cd08968b3feeac72867fc0c85431849ec98
Satissss <yihan3123@gmail.com>
2025-03-27 21:07:47 +0800

    Update EmbedModelPathMap.py

--- embed_model/EmbedModelPathMap.py
+++ embed_model/EmbedModelPathMap.py
@@ -1,5 +1,4 @@
 # 在这里添加模型和本地地址的映射
 embed_model_map_name_to_path = {
-    "BAAI/bge-large-zh": r"E:\pretrain_model\text_embedding\bge_large_zh",
-    "BAAI/bge-large-en-v1.5": r"E:\pretrain_model\text_embedding\bge_large_en_v15"
+    "BAAI/bge-large-en-v1.5": r".../bge_large_en_v15"
 }
